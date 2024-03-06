6d7aa70241467655f5154af40b6f6206ab15903a
Yousif <yusuf.babiker0@gmail.com>
2024-03-06 11:01:42 +0400

    [update] add googleMap Key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8082/',
+  apiUrl: 'https://localhost:7042/',
+  GoogleMapekey:'AIzaSyBaSZSAyKWTXzcPAC3lzi247ToUkgDLmvA'
 };
 
 /*
